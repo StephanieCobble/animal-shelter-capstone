e3d609268272ba43761ed22c550cd6f16826757c
StephanieCobble <cobble.stephanie@gmail.com>
2022-05-03 14:53:50 -0700

    added views with auth for employees.

--- backend/employees/views.py
+++ backend/employees/views.py
@@ -7,3 +7,41 @@ from .serializers import EmployeeSerializer
 from .models import Employee
 
 # Create your views here.
+class EmployeeList(APIView, AllowAny):
+    def get(self, request, format=None):
+        employee = Employee.objects.all()
+        serializer = EmployeeSerializer(employee, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        serializer = EmployeeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class EmployeeDetail(APIView, IsAuthenticated):
+    def get_object(self, pk):
+        try:
+            return Employee.objects.get(pk=pk)
+        except Employee.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk, format=None):
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk, format=None):    #updates
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def delete(self, request, pk, format=None):
+        employee = self.get_object(pk)
+        delete_conf = {
+            "Your account has been deleted. We're sad to see you go!": employee.first_name
+        }
+        employee.delete()
+        return Response(delete_conf, status=status.HTTP_200_OK)
