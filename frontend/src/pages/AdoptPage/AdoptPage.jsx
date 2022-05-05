91b03e5e74a0b8e0f563b7df57bdc82044d19b78
StephanieCobble <cobble.stephanie@gmail.com>
2022-05-05 14:51:13 -0700

    Filter dropdown in progress for adoptable pets

--- frontend/src/pages/AdoptPage/AdoptPage.jsx
+++ frontend/src/pages/AdoptPage/AdoptPage.jsx
@@ -3,9 +3,12 @@ import { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
 import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const AdoptPage = () => {
   const [pets, setPets] = useState([]);
+
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     const getAnimals = async () => {
@@ -20,27 +23,54 @@ const AdoptPage = () => {
   }, []);
 
   return (
-    <div>
-      {pets &&
-        pets.map((pet) => (
-          <p key={pet.id}>
-           <h3> Name: {pet.name} </h3>
-            {" "}
-            <h4> Species: {pet.species} </h4>
-            {" "}
-            <h4>Breed: {pet.breed} </h4>
-            {" "}
-            <h4>Male/Female: {pet.sex} </h4>
-            {" "}
-            <h4>Age: {pet.age} </h4>
-            {" "}
-            <h4>  {pet.image}</h4>
+    <div  >
+      <div className="container" >
+        
+        <div className="dropdown">
+          <button
+            className="btn btn-primary dropdown-toggle"
+            type="button"
+            data-toggle="dropdown"
+            
+          >
+            Filter
+            <span className="caret"></span>
+          </button>
+          <ul className="dropdown-menu">
+            <li>
+              <a href="#">HTML</a>
+            </li>
+            <li>
+              <a href="#">CSS</a>
+            </li>
+            <li>
+              <a href="#">JavaScript</a>
+            </li>
+          </ul>
+        </div>
+      </div>
 
-            {" "}
-            <h6>  {pet.description}</h6>
-           <p> {" "}</p>
-          </p>
-        ))}
+      <div>
+        {pets &&
+          pets
+            .filter((pet) => {
+              if (query === "") {
+                return pet;
+              } else if (pet.id.includes(query)) {
+                return pet;
+              }
+            })
+
+            .map((pet) => (
+              <p key={pet.id}>
+                <h3> Name: {pet.name} </h3> <h4> Species: {pet.species} </h4>{" "}
+                <h4>Breed: {pet.breed} </h4> <h4>Male/Female: {pet.sex} </h4>{" "}
+                <h4>Age: {pet.age} </h4> <h4> {pet.image}</h4>{" "}
+                <h6> {pet.description}</h6>
+                <p> </p>
+              </p>
+            ))}
+      </div>
     </div>
   );
 };
